436b14c85e436638566dd82567be454919f65bd5
Zsombor Udvardy <zsombor.udvardy@lensa.com>
2019-03-30 17:56:05 +0100

    Separated out the navigation component and modified router

---
+++ frontend/src/components/NavigationDrawer.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <v-navigation-drawer
+      v-model="drawer"
+      :clipped="$vuetify.breakpoint.lgAndUp"
+      fixed
+      app
+    >
+      <v-list dense>
+        <template v-for="item in items">
+          <v-layout
+            v-if="item.heading"
+            :key="item.heading"
+            row
+            align-center
+          >
+            <v-flex xs6>
+              <v-subheader v-if="item.heading">
+                {{ item.heading }}
+              </v-subheader>
+            </v-flex>
+            <v-flex xs6 class="text-xs-center">
+              <a href="#!" class="body-2 black--text">EDIT</a>
+            </v-flex>
+          </v-layout>
+          <v-list-tile v-else :key="item.text" :to="item.path">
+            <v-list-tile-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>
+                {{ item.text }}
+              </v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </template>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-toolbar
+      app
+      fixed
+      :clipped-left="$vuetify.breakpoint.lgAndUp"
+    >
+      <v-toolbar-title class="headline text-uppercase ml-0 pl-3">
+        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+        <span class="hidden-sm-and-down">Banky stuff</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        flat
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">Log out</span>
+      </v-btn>
+    </v-toolbar>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            drawer: null,
+            items: [
+                { icon: 'dashboard', text: 'Home', path: '/home'},
+                { icon: 'history', text: 'Frequently contacted', path: '/about'}
+            ]
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
