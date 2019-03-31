d7fdce0759ef137ec6842b33c066322fd8364961
Zsombor Udvardy <zsombor.udvardy@lensa.com>
2019-03-31 12:15:01 +0200

    Added dist under docs

---
+++ frontend/docs/precache-manifest.08f7450164ab4eaa32d84ee51f1a8ad1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "2634d7e815cc6b98c98e",
+    "url": "/js/team.c24de9f4.js"
+  },
+  {
+    "revision": "ece6d6dad0fa74d5c863",
+    "url": "/js/analysis.407f4995.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "38dd78a4db597e6ec21e",
+    "url": "/js/chunk-vendors.bd4ec0d6.js"
+  },
+  {
+    "revision": "53611dfbe82074876665",
+    "url": "/js/app.52b61727.js"
+  },
+  {
+    "revision": "230a77ac8bc2a0f2a559",
+    "url": "/js/history.efea5de3.js"
+  },
+  {
+    "revision": "abfbe79c1de374e401ecaf0c3f5477bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eaf24e3bd5dcb21f0bf59aa07e133730",
+    "url": "/example-avatar.png"
+  },
+  {
+    "revision": "2634d7e815cc6b98c98e",
+    "url": "/css/team.b800cc3d.css"
+  },
+  {
+    "revision": "230a77ac8bc2a0f2a559",
+    "url": "/css/history.b69081e5.css"
+  },
+  {
+    "revision": "38dd78a4db597e6ec21e",
+    "url": "/css/chunk-vendors.53737ad4.css"
+  },
+  {
+    "revision": "b02569de49093c07d0382b795f7ed91c",
+    "url": "/avatar-3.jpg"
+  },
+  {
+    "revision": "fae1126af03e5bb3ffcc7c36a90623bb",
+    "url": "/avatar-2.png"
+  },
+  {
+    "revision": "1200ad6a2c8d1c2197dbfb2edd59daae",
+    "url": "/avatar-1.jpg"
+  }
+];
