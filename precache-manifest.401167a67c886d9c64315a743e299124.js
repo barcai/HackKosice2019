402911371b0fe9454dcecc0321da14d3dba05a53
Zsombor Udvardy <zsombor.udvardy@lensa.com>
2019-03-31 13:33:07 +0200

    Initial dist subtree commit

---
+++ precache-manifest.401167a67c886d9c64315a743e299124.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d10917b77259d23aa65",
+    "url": "/HackKosice2019/js/team.216c74ba.js"
+  },
+  {
+    "revision": "579eaba1c5edb32a0fd7",
+    "url": "/HackKosice2019/js/analysis.44498e43.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/HackKosice2019/robots.txt"
+  },
+  {
+    "revision": "38dd78a4db597e6ec21e",
+    "url": "/HackKosice2019/js/chunk-vendors.bd4ec0d6.js"
+  },
+  {
+    "revision": "20ab816103dfb12a5383",
+    "url": "/HackKosice2019/js/app.98bac009.js"
+  },
+  {
+    "revision": "2f2f99fed67ff8187f80",
+    "url": "/HackKosice2019/js/history.2251c872.js"
+  },
+  {
+    "revision": "b0b262885dd6ad8e7c5202ceb53662aa",
+    "url": "/HackKosice2019/index.html"
+  },
+  {
+    "revision": "eaf24e3bd5dcb21f0bf59aa07e133730",
+    "url": "/HackKosice2019/example-avatar.png"
+  },
+  {
+    "revision": "9d10917b77259d23aa65",
+    "url": "/HackKosice2019/css/team.b800cc3d.css"
+  },
+  {
+    "revision": "2f2f99fed67ff8187f80",
+    "url": "/HackKosice2019/css/history.b69081e5.css"
+  },
+  {
+    "revision": "38dd78a4db597e6ec21e",
+    "url": "/HackKosice2019/css/chunk-vendors.53737ad4.css"
+  },
+  {
+    "revision": "b02569de49093c07d0382b795f7ed91c",
+    "url": "/HackKosice2019/avatar-3.jpg"
+  },
+  {
+    "revision": "fae1126af03e5bb3ffcc7c36a90623bb",
+    "url": "/HackKosice2019/avatar-2.png"
+  },
+  {
+    "revision": "1200ad6a2c8d1c2197dbfb2edd59daae",
+    "url": "/HackKosice2019/avatar-1.jpg"
+  }
+];
